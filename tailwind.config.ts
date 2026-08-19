16389c452495505136ab20cb73445d898dc4b57c
07-khanh <khanh.npn07@gmail.com>
2026-08-20 01:11:14 +0700

    Add Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)', 'sans-serif'],
+        serif: ['var(--font-serif)', 'serif'],
+        mono: ['var(--font-mono)', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
